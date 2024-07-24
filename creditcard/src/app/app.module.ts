c17e8a52140f9cbdb81faaa71ed6eb55adb04ea9
Huzefa Battiwala <huzaifa.hb.123@gmail.com>
2024-07-25 00:45:26 +0500

    angular reactive forms

---
+++ creditcard/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
+
+@NgModule({
+  declarations: [AppComponent, CardFormComponent, InputComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ creditcard/src/app/card-form/card-form.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-card-form',
+  templateUrl: './card-form.component.html',
+  styleUrl: './card-form.component.css',
+})
+export class CardFormComponent {
+  cardForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+
+    cardNumber: new FormControl(''),
+
+    expiration: new FormControl(''),
+
+    securityCode: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(this.cardForm.value);
+  }
+}
