c50dec859c069c2333c40cd68b18c284c0c55804
Huzefa Battiwala <huzaifa.hb.123@gmail.com>
2024-07-26 00:18:21 +0500

    input masking

--- creditcard/src/app/card-form/card-form.component.ts
+++ creditcard/src/app/card-form/card-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DateFormControl } from '../date-form-control';
 
 @Component({
   selector: 'app-card-form',
@@ -10,14 +11,29 @@ export class CardFormComponent {
   cardForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
 
-    cardNumber: new FormControl(''),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
 
-    expiration: new FormControl(''),
+    expiration: new DateFormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/),
+    ]),
 
-    securityCode: new FormControl(''),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+    ]),
   });
 
   onSubmit() {
     console.log(this.cardForm.value);
+  }
+
+  onResetClick() {
+    this.cardForm.reset();
   }
 }
